ed5a2fe0c91ee96b6abd1496fd3cbb6d25b58ccf
Sahla <sahlaa.shamz@gmail.com>
2026-03-23 18:11:58 +0530

    [ADD] 23/03/2026 weather notification module

---
+++ weather_notification/__manifest__.py
@@ -0,0 +1,16 @@
+{
+
+    "name" : "Weather Notification",
+    "version" : "1.0",
+    "author" : "",
+    "depends": [
+        "base", "point_of_sale"
+    ],
+    "assets" : {
+        'web.assets_backend' : [
+            'weather_notification/static/src/xml/weather_info.xml',
+            "weather_notification/static/src/js/weather_comp.js",
+
+        ]
+    }
+}
