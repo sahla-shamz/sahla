17cecc9f2aa48ccfabeb42a5e2c364900d6b798d
Sahla <sahlaa.shamz@gmail.com>
2026-03-27 17:17:13 +0530

    [ADD] 27/03/2026 salesperson filter module second commit

--- salesperson_filter/__manifest__.py
+++ salesperson_filter/__manifest__.py
@@ -3,15 +3,15 @@
     "version" : "19.0.1.0.0",
     "description" : "select salesperson and filter the crm list view",
     "depends" : [
-        "crm"
+        "crm","web"
     ],
     "data" : [
         "views/salesperson_filter.xml"
     ],
     "assets" : {
         "web.assets_backend" : [
-"salesperson_filter/static/src/js/list_view_crm.js",
-            "salesperson_filter/static/src/xml/list_view_crm.xml",
+        "salesperson_filter/static/src/js/list_view_crm.js",
+        "salesperson_filter/static/src/xml/list_view_crm.xml",
 
         ]
     }
--- salesperson_filter/static/src/js/list_view_crm.js
+++ salesperson_filter/static/src/js/list_view_crm.js
@@ -1,25 +1,85 @@
+/** @odoo-module **/
 import { registry } from '@web/core/registry';
 import { listView } from '@web/views/list/list_view';
-import { ListController } from '@web/views/list/list_controller';
+//import { ListController } from '@web/views/list/list_controller';
+//import { ControlPanel } from "@web/search/control_panel/control_panel";
+import { ListRenderer } from "@web/views/list/list_renderer";
+import { onWillStart, useState, onMounted, onWillRender } from '@odoo/owl';
+import { user } from "@web/core/user";
+import { useService } from "@web/core/utils/hooks";
 
 
-export class CrmListController extends ListController
-    {
-        super(){
-        console.log("helloo")
-            super.setup()
-            this.DemoFunction()
-        }
 
-       async DemoFunction()
-       {
-            console.log("demooo")
-       }
+
+export class CrmListRenderer extends ListRenderer {
+    static template = "salesperson_filter.CrmListTemplate";
+
+    setup() {
+        super.setup();
+        this.orm = useService("orm")
+        this.action = useService("action");
+        this.state = useState({ salesperson: [] });
+
+        onWillStart(async ()=>{
+            await this.get_users()
+        })
+
+
+
     }
 
 
-CrmListController.template = "salesperson_filter.CrmListTemplate"
-registry.category('views').add('crm_lead_list', {
+    async get_users(){
+        console.log(user)
+        console.log("data", this.props)
+        this.state.salesperson = await this.orm.searchRead("res.users", [] , ["id", "name"])
+        console.log(this.state.salesperson)
+        console.log("contextttt",this)
+//        this.action.doAction({
+//                            type: "ir.actions.act_window",
+//                            res_model: "crm.lead",
+//                            views: [
+//                                [false, "list"],
+//                                [false, "form"],
+//                            ],
+//                            domain : [['user_id' , '=' , this.state.salesperson.id]]
+//                        })
+
+
+    }
+
+
+    onChange(){
+        console.log("hbbbbbbbbbd")
+        console.log(this.state.salesperson.id)
+        console.log(this.action.currentAction.then())
+//         this.action.doAction({
+//                type: "ir.actions.act_window",
+//                res_model: "crm.lead",
+//                views: [
+//                    [false, "list"],
+//                    [false, "form"],
+//                ],
+//                domain : [['user_id' , 'in' , [Number(this.state.salesperson.id)]]]
+//            })
+    }
+
+
+
+//    static components = {
+//        ...ListRenderer.components,
+//
+//    };
+};
+
+export const crmListView = {
     ...listView,
-    Controller: CrmListController,
-});
+    Renderer: CrmListRenderer
+};
+
+registry.category("views").add("crm_lead_list", crmListView);
+
+
+
+
+
