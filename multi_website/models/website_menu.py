28408207facf56a97f1b0df5e729edefd51faf8b
Sahla <sahlaa.shamz@gmail.com>
2026-04-10 17:23:30 +0530

    [ADD]10/04/2026 multi website setup, content configurtion and cleanup in odoo

---
+++ multi_website/models/website_menu.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class WebsiteMenu(models.Model):
+    _inherit = "website.menu"
+
+
+    active = fields.Boolean(default=True, string="Active")
