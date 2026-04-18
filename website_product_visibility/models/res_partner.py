ca37cccb18656ce037f56124b7efb63fed37ca0f
Sahla <sahlaa.shamz@gmail.com>
2026-04-18 16:50:39 +0530

    [ADD] 18/04/2026 Website Product visibility based on the logged-in users products and categories

--- website_product_visibility/models/res_partner.py
+++ website_product_visibility/models/res_partner.py
@@ -1,8 +1,33 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+
+    select_pro_cat= fields.Selection([('products', "Products"), ('category', "Product Category")], string="Product or Category")
     product_ids = fields.Many2many("product.template", string="Products")
+    product_category_ids = fields.Many2many("product.public.category", string="Categories", domain="[['parent_id', '=', False]]")
+    is_product = fields.Boolean(string="Is Product", compute="_compute_is_product")
+    is_category = fields.Boolean(string="Is Category")
+
+
+    @api.depends('select_pro_cat')
+    def _compute_is_product(self):
+        for rec in self:
+            if rec.select_pro_cat == 'products':
+                self.update({
+                    'product_category_ids' : [fields.Command.clear()]
+                })
+                rec.is_product = True
+                rec.is_category = False
+            elif rec.select_pro_cat == 'category':
+                self.update({
+                    'product_ids': [fields.Command.clear()]
+                })
+                rec.is_category = True
+                rec.is_product = False
+            else:
+                rec.is_product = False
+                rec.is_category = False
 
 
