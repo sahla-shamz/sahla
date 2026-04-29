0be38395049fe3e1dc0f0f35b6ef3c6acc2f1608
Sahla <sahlaa.shamz@gmail.com>
2026-04-29 17:23:44 +0530

    [ADD] 29/04/2026 Website Add to cart Quantity Initial Commit

---
+++ website_add_to_cart_quantity/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name" : "Website Add to Cart Quantity",
+    "summary" : "Website Add to Cart Quantity",
+    "version" : "19.0.1.0.0",
+    "depends" : [
+        "base", "website_sale"
+    ],
+    "data" : [
+        "views/website_product.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False
+}
