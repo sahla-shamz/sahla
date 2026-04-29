b3ae2867c3ed3c481f769624348c2b33e472eeae
Sahla <sahlaa.shamz@gmail.com>
2026-04-29 17:14:12 +0530

    [ADD] 29/04/2026 PoS Order Types in Version 19 final commit

--- pos_order_types/__manifest__.py
+++ pos_order_types/__manifest__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 {
     "name" : "POS Order Types",
     "version" : "19.0.1.0.0",
@@ -6,16 +8,18 @@
     ],
     "data" : [
         "security/ir.model.access.csv",
+        "data/order_type_data.xml",
         "views/pos_conf.xml"
     ],
     "assets": {
         'point_of_sale._assets_pos': [
-            "pos_order_types/static/src/app/components/info_popup.xml",
-            "pos_order_types/static/src/app/components/info_popup.js",
             "pos_order_types/static/src/js/control_button.js",
             "pos_order_types/static/src/xml/control_button_inherit.xml",
             "pos_order_types/static/src/xml/order_reciept_inherit.xml"
 
         ]
-    }
+    },
+    "installable": True,
+    "auto_install": False,
+    "application" : False
 }
