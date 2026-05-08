b2bc328e813902426e1aad959fe0fa2934412dd3
Sahla <sahlaa.shamz@gmail.com>
2026-05-08 17:17:50 +0530

    [ADD] 08/05/2026 Pos Sale Order second commit

--- pos_sale_order/models/sale_order.py
+++ pos_sale_order/models/sale_order.py
@@ -4,8 +4,36 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
+    is_created_from_pos = fields.Boolean("Created from poS", readonly=True)
+    pos_session_id = fields.Many2one("pos.session", string="POS Session", readonly=True)
 
-    def create_sale_order(self, option_value):
-        print("hhhhh", option_value)
 
-        return True
+    @api.model
+    def create_sale_order(self, state, product_id, partner_id, note, session):
+        print("hhhhh", state)
+        print("hhhhh", product_id)
+        print("hhhhh", partner_id)
+
+
+        sale_order = self.create({
+            'partner_id': partner_id,
+            'state' : state,
+            'note' : note,
+            'is_created_from_pos' : True,
+            'pos_session_id' : session,
+            'order_line' : [fields.Command.create({
+                'product_id': pro['id'],
+                'product_uom_qty': pro['qty'],
+                'price_unit': pro['price'],
+                'tax_ids' : [fields.Command.set(pro['tax'])],
+            }) for pro in product_id],
+
+
+        })
+
+
+        print('sale Order', sale_order)
+
+
+
+        return sale_order.name
--- pos_sale_order/static/src/app/components/infopopup.js
+++ pos_sale_order/static/src/app/components/infopopup.js
@@ -2,7 +2,8 @@ import { Dialog } from "@web/core/dialog/dialog";
 import { usePos } from "@point_of_sale/app/hooks/pos_hook";
 import {Component, useState} from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
-
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { _t } from "@web/core/l10n/translation";
 
 
 export class InfoPopup extends Component {
@@ -10,8 +11,9 @@ export class InfoPopup extends Component {
      static components = { Dialog };
      setup(){
         this.pos= usePos();
-        this.state = useState({ values : 'draft' })
+        this.state = useState({ values : 'draft', desc : "" })
          this.orm = useService("orm");
+        this.dialog = useService("dialog")
      }
 
      async discard() {
@@ -20,10 +22,63 @@ export class InfoPopup extends Component {
 
 
     async createSaleOrder() {
-         console.log("jijijiji", this.state.values)
-        const items = await this.orm.call("sale.order", "create_sale_order",[this.state.values], {});
+
+        const session = this.pos.openOrder.session_id.id
+        const description = this.state.desc
+        const status = this.state.values
+        const order_lines = this.pos.openOrder.lines
+        console.log("order liness", order_lines)
+        console.log("pos", this.pos)
+        var arr_products = []
+        for(const i of order_lines)
+        {
+            // console.log("iiiiiiiiii", i.tax_ids)
+
+            var taxes =[]
+            for (const j of i.tax_ids)
+            {
+                taxes.push(j.id)
+                // console.log("kkkkkkkkkkk",j)
+            }
+
+            console.log("taxes", taxes)
+
+            arr_products.push({'id' : i.product_id.id,
+                            "qty" : i.quantityStr.unitPart,
+                            'price' : i.displayPriceUnitExcl,
+                            'tax' : taxes})
+
+        }
+        console.log("arrrrr", arr_products)
+
+        if (! this.pos.openOrder.partner_id)
+        {
+            this.props.close()
+            this.dialog.add(AlertDialog, {
+                title : _t("Customer Required"),
+                body: _t("Please Select Customer"),
+            });
+        }
+        else if(! order_lines.length)
+        {
+            this.props.close()
+            this.dialog.add(AlertDialog, {
+                title : _t("Empty Order"),
+                body: _t("Please Add some Products."),
+            });
+        }
+        else {
+            var partner = this.pos.openOrder.partner_id.id
+            const order_ref = await this.orm.call("sale.order", "create_sale_order",[status, arr_products, partner, description, session], {});
+
+            this.props.close()
+            this.dialog.add(AlertDialog, {
+                title : _t("Sale Order Created"),
+                body: _t(order_ref),
+            });
+        }
+
+
     }
-
-
 
 }
